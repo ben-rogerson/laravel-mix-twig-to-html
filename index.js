b179ddbeb646b9127d985d40da3fa65645ca2f83
Ben Rogerson <info@benrogerson.com.au>
2019-08-15 18:10:42 +0930

    add advanced file configuration

--- index.js
+++ index.js
@@ -1,89 +1,123 @@
-const mix = require("laravel-mix")
-const path = require("path")
+const mix = require('laravel-mix');
+const path = require('path');
 
 class TwigToHtml {
-    dependencies() {
-        return ["html-webpack-plugin", "raw-loader", "twig-html-loader"]
+  dependencies() {
+    return ['html-webpack-plugin', 'raw-loader', 'twig-html-loader'];
+  }
+
+  register(config) {
+    if (!config.files || config.files.length <= 0) {
+      throw new Error(
+        `Missing files\nEg: mix.twigToHtml({ files: ['path/to/twigfile.twig'] })`
+      );
     }
-
-    register(config) {
-        if (!config.files || config.files.length <= 0) {
-            throw new Error(
-                `Missing files\nEg: mix.twigToHtml({ files: ['path/to/twigfile.twig'] })`
-            )
-        }
-        if (!config.fileBase) {
-            throw new Error(
-                `Missing fileBase\nEg: mix.twigToHtml({ fileBase: ['path/to/your/twig/templates'] })`
-            )
-        }
-        this.config = Object.assign(
-            {
-                files: [],
-                fileBase: undefined,
-                twigOptions: null,
-                enabled: true,
-            },
-            config
-        )
+    if (!config.fileBase) {
+      throw new Error(
+        `Missing fileBase\nEg: mix.twigToHtml({ fileBase: ['path/to/your/twig/templates'] })`
+      );
     }
+    this.config = Object.assign(
+      {
+        files: [],
+        fileBase: undefined,
+        twigOptions: null,
+        enabled: true
+      },
+      config
+    );
+  }
 
-    webpackRules() {
-        if (!this.config.enabled) return
+  webpackRules() {
+    if (!this.config.enabled) return;
 
-        const options = Object.assign({
-            autoescape: true,
-            functions: {},
-        },
-            this.config.twigOptions
-        )
+    const options = Object.assign(
+      {
+        autoescape: true,
+        functions: {}
+      },
+      this.config.twigOptions
+    );
 
+    return {
+      test: /\.twig$/,
+      use: [
+        'raw-loader',
+        {
+          loader: 'twig-html-loader',
+          options: options
+        }
+      ]
+    };
+  }
+
+  webpackPlugins() {
+    if (!this.config.enabled) return;
+
+    const HtmlWebpackPlugin = require('html-webpack-plugin');
+    const globby = require('globby');
+
+    const normaliseFileConfig = files =>
+      typeof files[0] === 'string'
+        ? globby.sync(files).map(file => ({ template: file }))
+        : typeof files[0] === 'object'
+        ? Object.values(files).reduce((prev, fileConfig) => {
+            const paths = globby.sync(fileConfig.template).map(file => ({
+              ...fileConfig,
+              template: file
+            }));
+            return prev.concat(paths);
+          }, [])
+        : [];
+
+    const removeUnderscorePaths = config =>
+      config.filter(
+        item =>
+          item.template
+            .split('/')
+            .map(chunk => chunk.startsWith('_'))
+            .filter(Boolean).length === 0
+      );
+
+    const addFilename = config =>
+      config.map(item => {
+        const isSubPath = this.config.fileBase !== path.dirname(item.template);
+        const prefixPath = isSubPath
+          ? path
+              .dirname(item.template)
+              .split(path.sep)
+              .pop()
+          : '';
+        const newFileName = `${path.basename(
+          item.template,
+          path.extname(item.template)
+        )}.html`;
         return {
-            test: /\.twig$/,
-            use: [
-                "raw-loader",
-                {
-                    loader: "twig-html-loader",
-                    options: options,
-                },
-            ],
-        }
-    }
+          ...item,
+          filename: path.join(prefixPath, newFileName)
+        };
+      });
 
-    webpackPlugins() {
-        if (!this.config.enabled) return
+    const createPages = pages =>
+      pages.map(
+        page =>
+          new HtmlWebpackPlugin({
+            ...page,
+            hash: mix.inProduction()
+          })
+      );
 
-        const HtmlWebpackPlugin = require("html-webpack-plugin")
-        const files =
-            typeof this.config.files === "object"
-                ? this.config.files
-                : [this.config.files]
+    const pluginConfig = createPages(
+      addFilename(removeUnderscorePaths(normaliseFileConfig(this.config.files)))
+    );
 
-        const templatePages = files.map(file => {
-            const isSubPath = this.config.fileBase !== path.dirname(file)
-            const prefixPath = isSubPath
-                ? path
-                    .dirname(file)
-                    .split(path.sep)
-                    .pop()
-                : ""
-            const newFileName = `${path.basename(
-                file,
-                path.extname(file)
-            )}.html`
-            return new HtmlWebpackPlugin({
-                template: file,
-                filename: path.join(prefixPath, newFileName),
-                hash: mix.inProduction(),
-            })
-        })
-        return templatePages
-    }
+    return pluginConfig;
+  }
 
-    webpackConfig(webpackConfig) {
-        if (!this.config.enabled) return
-        webpackConfig.output.publicPath = "" // Fix path issues
-    }
+  webpackConfig(webpackConfig) {
+    if (!this.config.enabled) return;
+    webpackConfig.output.publicPath = ''; // Fix path issues
+  }
 }
 
-mix.extend("twigToHtml", new TwigToHtml())
+mix.extend('twigToHtml', new TwigToHtml());
